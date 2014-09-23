59a8b68ffd8773e280549fe8c326bc1c735cb830
Guido Espinosa <guido@superG.co>
2014-09-23 12:59:38 -0500

    compatibility for Django version>1.5
    removes usage of deprecated urls.defaults module when Django version > 1.5. Checks Django version so it remains backwards compatible.

--- follow/urls.py
+++ follow/urls.py
@@ -1,4 +1,9 @@
-from django.conf.urls.defaults import *
+from django import VERSION as DjangoVersion
+if float('%s.%s' % DjangoVersion[:2]) > 1.5:
+    from django.conf.urls import patterns, url
+else:
+    from django.conf.urls.defaults import *
+
 
 urlpatterns = patterns('',
     url(r'^toggle/(?P<app>[^\/]+)/(?P<model>[^\/]+)/(?P<id>\d+)/$', 'follow.views.toggle', name='toggle'),
